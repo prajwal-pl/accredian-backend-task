14469ba80b070dab57ac6b94300f19f43098c4b7
Prajwal-PL <prajwalpl096@gmail.com>
2024-07-06 19:55:47 +0530

    init

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,16 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("Server is running on port 3000");
 });
