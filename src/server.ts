05ca8e7adb0325e46d08aa00fb99ab229c5af3a3
Prajwal-PL <prajwalpl096@gmail.com>
2024-07-06 16:55:44 +0530

    initialised an express server

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
